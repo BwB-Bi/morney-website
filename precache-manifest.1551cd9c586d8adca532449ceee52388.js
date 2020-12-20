8538da07f12a8f06b7ea62af833db9bc9d1e3685
joco <13711619148@163.com>
2020-12-20 18:49:15 +0800

    deploy

---
+++ precache-manifest.1551cd9c586d8adca532449ceee52388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be5badb8d25f07938528178e20b6bca",
+    "url": "/morney-website/QRcode.png"
+  },
+  {
+    "revision": "ccf0475399e626251f19",
+    "url": "/morney-website/css/app.cb2c40a9.css"
+  },
+  {
+    "revision": "6fc26a3e0d9c875353faef91ab370205",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "ccf0475399e626251f19",
+    "url": "/morney-website/js/app.c924c03c.js"
+  },
+  {
+    "revision": "e92896ad7a9837b15835",
+    "url": "/morney-website/js/chunk-vendors.67439347.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
